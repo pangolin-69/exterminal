424e1e3ad06f7a83819157b47f29d9c7832f3e2a
Jürgen Landwehr <juergen.landwehr@daimler.com>
2021-03-03 22:50:39 +0100

    update 3

--- src/main.go
+++ src/main.go
@@ -1,30 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"golang.org/x/term"
-	"os"
-	"strings"
+	"github.com/pangolin-69/exterminal/src/prompter"
 )
 
 func main() {
-	username, password := credentials()
+	username, password := prompter.Credentials()
 	fmt.Printf("Username: %s, Password: %s\n", username, password)
-}
-
-func credentials() (string, string) {
-	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Print("Enter Username: ")
-	username, _ := reader.ReadString('\n')
-
-	fmt.Print("Enter Password: ")
-	bytePassword, err := term.ReadPassword(0)
-	if err == nil {
-		fmt.Println("\nPassword typed: " + string(bytePassword))
-	}
-	password := string(bytePassword)
-
-	return strings.TrimSpace(username), strings.TrimSpace(password)
 }
---
+++ src/prompter/prompter.go
@@ -0,0 +1,25 @@
+package prompter
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/term"
+	"os"
+	"strings"
+)
+
+func Credentials() (string, string) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter Username: ")
+	username, _ := reader.ReadString('\n')
+
+	fmt.Print("Enter Password: ")
+	bytePassword, err := term.ReadPassword(0)
+	if err == nil {
+		fmt.Println("\nPassword typed: " + string(bytePassword))
+	}
+	password := string(bytePassword)
+
+	return strings.TrimSpace(username), strings.TrimSpace(password)
+}
